e1a39fbee0e990bff82307d451a490e3ce2f4fda
joaoalex <joao@localhost.(none)>
2017-11-08 22:48:26 +0000

    ola

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-(function($) {
-
-  var app = $.sammy('#app', function() {
-    this.use('Template');
-
-    this.around(function(callback) {
-      var context = this;
-      this.load('data/articles.json')
-          .then(function(items) {
-            context.items = items;
-          })
-          .then(callback);
-    });
-
-    this.get('#/', function(context) {
-      context.app.swap('');
-      $.each(this.items, function(i, item) {
-        context.render('templates/article.template', {id: i, item: item})
-               .appendTo(context.$element());
-      });
-    });
+$(document).on('pageinit', '#home', function(){      
+    var url = 'http://api.themoviedb.org/3/',
+        mode = 'search/movie?query=',
+        movieName = '&query='+encodeURI('Batman'),        
+        key = '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';        
     
-    this.get('#/about/', function(context) {
-        var str=location.href.toLowerCase();
-        context.app.swap('');
-        context.render('templates/about.template', {})
-               .appendTo(context.$element());
-    });
+    $.ajax({
+        url: url + mode + key + movieName ,
+        dataType: "jsonp",
+        async: true,
+        success: function (result) {
+            ajax.parseJSONP(result);
+        },
+        error: function (request,error) {
+            alert('Network error has occurred please try again!');
+        }
+    });         
+});
 
-    this.get('#/article/:id', function(context) {
-      this.item = this.items[this.params['id']];
-      if (!this.item) { return this.notFound(); }
-      this.partial('templates/article-detail.template');
-    });
+$(document).on('pagebeforeshow', '#headline', function(){      
+    $('#movie-data').empty();
+    $.each(movieInfo.result, function(i, row) {
+        if(row.id == movieInfo.id) {
+            $('#movie-data').append('<li><img src="http://image.tmdb.org/t/p/w92'+row.poster_path+'"></li>');
+            $('#movie-data').append('<li>Title: '+row.original_title+'</li>');
+            $('#movie-data').append('<li>Release date'+row.release_date+'</li>');
+            $('#movie-data').append('<li>Popularity : '+row.popularity+'</li>');   
+            $('#movie-data').append('<li>Popularity : '+row.vote_average+'</li>');             
+            $('#movie-data').listview('refresh');            
+        }
+    });    
+});
 
+$(document).on('vclick', '#movie-list li a', function(){  
+    movieInfo.id = $(this).attr('data-id');
+    $.mobile.changePage( "#headline", { transition: "slide", changeHash: false });
+});
 
-    this.before('.*', function() {
+var movieInfo = {
+    id : null,
+    result : null
+}
 
-        var hash = document.location.hash;
-        $("nav").find("a").removeClass("current");
-        $("nav").find("a[href='"+hash+"']").addClass("current");
-   });
+var ajax = {  
+    parseJSONP:function(result){  
+        movieInfo.result = result.results;
+        $.each(result.results, function(i, row) {
+            console.log(JSON.stringify(row));
+            $('#movie-list').append('<li><a href="" data-id="' + row.id + '"><img src="http://image.tmdb.org/t/p/w92'+row.poster_path+'"/><h3>' + row.title + '</h3><p>' + row.vote_average + '/10</p></a></li>');
+        });
+        $('#movie-list').listview('refresh');
+    }
+}
 
-  });
-
-  $(function() {
-    app.run('#/about/');
-  });
-
-})(jQuery);
